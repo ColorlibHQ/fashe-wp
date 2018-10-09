39dc60048c450cfe025ad326bed9072f80a632dd
codepixar <akash@spondonit.com>
2018-10-09 15:25:01 +0600

    Add epsilon submodule and fixed elementor default element style

---
+++ inc/customizer/fields/sections.php
@@ -0,0 +1,119 @@
+<?php 
+/**
+ * @Packge     : Fashe
+ * @Version    : 1.0
+ * @Author     : Colorlib
+ * @Author URI : http://colorlib.com/wp/
+ *
+ * Customizer panels and sections
+ *
+ */
+
+/***********************************
+ * Register customizer panels
+ ***********************************/
+
+$panels = array(
+    /**
+     * Theme Options Panel
+     */
+    array(
+        'id'   => 'fashe_options_panel',
+        'args' => array(
+            'priority'       => 0,
+            'capability'     => 'edit_theme_options',
+            'theme_supports' => '',
+            'title'          => esc_html__( 'Theme Options', 'fashe' ),
+        ),
+    )
+);
+
+
+/***********************************
+ * Register customizer sections
+ ***********************************/
+
+
+$sections = array(
+    /**
+     * General Section
+     */
+    array(
+        'id'   => 'fashe_general_options_section',
+        'args' => array(
+            'title'    => esc_html__( 'General', 'fashe' ),
+            'panel'    => 'fashe_options_panel',
+            'priority' => 1,
+        ),
+    ),
+    /**
+     * Header Section
+     */
+    array(
+        'id'   => 'fashe_headertop_options_section',
+        'args' => array(
+            'title'    => esc_html__( 'Header Top', 'fashe' ),
+            'panel'    => 'fashe_options_panel',
+            'priority' => 2,
+        ),
+    ),
+    /**
+     * Blog Section
+     */
+    array(
+        'id'   => 'fashe_blog_options_section',
+        'args' => array(
+            'title'    => esc_html__( 'Blog', 'fashe' ),
+            'panel'    => 'fashe_options_panel',
+            'priority' => 3,
+        ),
+    ),
+    /**
+     * WooCommerce Section
+     */
+    array(
+        'id'   => 'fashe_woocommerce_options_section',
+        'args' => array(
+            'title'    => esc_html__( 'WooCommerce', 'fashe' ),
+            'panel'    => 'fashe_options_panel',
+            'priority' => 4,
+        ),
+    ),
+
+    /**
+     * 404 Page Section
+     */
+    array(
+        'id'   => 'fashe_fof_options_section',
+        'args' => array(
+            'title'    => esc_html__( '404 Page', 'fashe' ),
+            'panel'    => 'fashe_options_panel',
+            'priority' => 5,
+        ),
+    ),
+    /**
+     * Footer Section
+     */
+    array(
+        'id'   => 'fashe_footer_options_section',
+        'args' => array(
+            'title'    => esc_html__( 'Footer', 'fashe' ),
+            'panel'    => 'fashe_options_panel',
+            'priority' => 6,
+        ),
+    ),
+
+);
+
+
+/***********************************
+ * Add customizer elements
+ ***********************************/
+$collection = array(
+    'panel'   => $panels,
+    'section' => $sections,
+);
+
+Epsilon_Customizer::add_multiple( $collection );
+
+?>
---
+++ inc/customizer/js/customizer.js
@@ -0,0 +1,37 @@
+(function( $ , api){
+
+    // Customizer about page redirect
+    api.section( 'fashe_fof_options_section' , function( section ){
+
+        section.expanded.bind( function( isExpanded ){
+
+            if( isExpanded ){
+                api.previewer.previewUrl.set( api.settings.url.home+'/maybe404page' );
+            }else{
+                api.previewer.previewUrl.set( api.settings.url.home );
+            }
+            
+        } )
+
+    } );
+
+    // Customizer blog page redirect
+    api.section( 'fashe_blog_options_section' , function( section ){
+
+        section.expanded.bind( function( isExpanded ){
+
+            if( isExpanded ){
+                api.previewer.previewUrl.set( moahCustomizerdata.blog_page );
+            }else{
+                api.previewer.previewUrl.set( api.settings.url.home );
+            }
+            
+
+
+        } )
+
+    } );
+
+
+
+})( jQuery, wp.customize );
