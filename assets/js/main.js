44788ccbedcf660b299e6f95b89ebfcde8314e68
codepixar <akash@spondonit.com>
2018-10-21 14:52:28 +0600

    Fixed theme color change issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -221,6 +221,79 @@
 		
 	});
 	
-	
+        /*[ Slick3 ]
+    ===========================================================*/
+
+    var slick3 = $('.slick3');
+
+    if( slick3.length ){
+
+        slick3.slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            dots: true,
+            appendDots: $('.wrap-slick3-dots'),
+            dotsClass:'slick3-dots',
+            infinite: true,
+            autoplay: false,
+            autoplaySpeed: 6000,
+            arrows: false,
+            customPaging: function(slick, index) {
+                var portrait = $(slick.$slides[index]).data('thumb');
+                return '<img src=" ' + portrait + ' "/><div class="slick3-dot-overlay"></div>';
+            },  
+        });
+        
+    }
+
+    /*[ Slick2 ]
+    ===========================================================*/
+    if( $('.relateproduct').length ){
+
+        $('.slick2').slick({
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            autoplay: false,
+            autoplaySpeed: 6000,
+            arrows: true,
+            appendArrows: $('.wrap-slick2'),
+            prevArrow:'<button class="arrow-slick2 prev-slick2"><i class="fa  fa-angle-left" aria-hidden="true"></i></button>',
+            nextArrow:'<button class="arrow-slick2 next-slick2"><i class="fa  fa-angle-right" aria-hidden="true"></i></button>',  
+            responsive: [
+                {
+                  breakpoint: 1200,
+                  settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+                  }
+                },
+                {
+                  breakpoint: 992,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                  }
+                },
+                {
+                  breakpoint: 768,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                  }
+                },
+                {
+                  breakpoint: 576,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+            ]    
+        });
+
+    }
+
 
 })(jQuery);
