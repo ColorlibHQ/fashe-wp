046a434ebc609522ca6ae3c3a2d3e59f73da96c9
unknown <cristi.ecmecian@yahoo.com>
2018-10-30 13:28:58 +0200

    templates folder - phpcs fixes

--- templates/content-none.php
+++ templates/content-none.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 // Block direct access
-if( !defined( 'ABSPATH' ) ){
+if ( ! defined( 'ABSPATH' ) ) {
 	exit( 'Direct script access denied.' );
 }
 /**
@@ -16,31 +16,31 @@ if( !defined( 'ABSPATH' ) ){
 
 	<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-	    <p><?php echo sprintf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'fashe' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+		<p><?php echo sprintf( esc_html__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'fashe' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 	<?php elseif ( is_search() ) : ?>
-		
-	    <p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'fashe' ); ?></p>
-	    	<div class="row content-none-search">
+
+		<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'fashe' ); ?></p>
+			<div class="row content-none-search">
 				<div class="col-sm-4 offset-sm-4 p-t-30">
-					<?php 
+					<?php
 					$anchor = fashe_anchor_tag(
 						array(
-							'url' 	 => esc_url( site_url( '/' ) ),
-							'text' 	 => esc_html__( 'Back to home page', 'fashe' ),
-							'class'  => 'flex-c-m bg4 size2 bo-rad-23 hov1 m-text3 trans-0-4'
+							'url'   => esc_url( site_url( '/' ) ),
+							'text'  => esc_html__( 'Back to home page', 'fashe' ),
+							'class' => 'flex-c-m bg4 size2 bo-rad-23 hov1 m-text3 trans-0-4',
 						)
 					);
 					echo wp_kses_post( $anchor );
 					?>
 				</div>
 			</div>
-			
+
 	<?php else : ?>
 
-	    <p><?php wp_kses_post( _e( 'It seems we can\'t find what you\'re looking for. Perhaps searching can help.', 'fashe' ) ); ?></p>
+		<p><?php esc_html_e( 'It seems we can\'t find what you\'re looking for. Perhaps searching can help.', 'fashe' ); ?></p>
 
-	    <?php get_search_form(); ?>
+		<?php get_search_form(); ?>
 
 	<?php endif; ?>
 </div>
--- templates/header-top3.php
+++ templates/header-top3.php
@@ -3,47 +3,47 @@
 	<!-- Header desktop -->
 	<div class="container-menu-header-v3">
 		<div class="wrap_header3 p-t-74">
-			<?php 
+			<?php
 			// Header Logo3
-			echo fashe_theme_logo('logo3');
+			echo fashe_theme_logo( 'logo3' );
 			?>
 
 			<!-- Header Icon -->
 			<div class="header-icons3 p-t-38 p-b-60 p-l-8">
-				<?php 
+				<?php
 				// Header login Icon
-				$loginUrl = fashe_opt( 'fashe_login_url' );
-				$cart = fashe_opt( 'fashe-cart-toggle-settings' );
-				
-				if( $loginUrl ):
-				?>
-				<a href="<?php echo esc_url( $loginUrl ); ?>" class="header-wrapicon1 dis-block">
+				$login_url = fashe_opt( 'fashe_login_url' );
+				$cart     = fashe_opt( 'fashe-cart-toggle-settings' );
+
+				if ( $login_url ) :
+					?>
+				<a href="<?php echo esc_url( $login_url ); ?>" class="header-wrapicon1 dis-block">
 					<img src="<?php echo esc_url( FASHE_DIR_ASSETS_URI ); ?>img/icons/icon-header-01.png" class="header-icon1" alt="<?php esc_html_e( 'ICON', 'fashe' ); ?>">
 				</a>
-				<?php 
+					<?php
 				endif;
-				if( $cart && $loginUrl ){
+				if ( $cart && $login_url ) {
 					echo '<span class="linedivide1"></span>';
 				}
 				// Cart
-				if( $cart ){
+				if ( $cart ) {
 					fashe_cart_count();
 				}
 				?>
 			</div>
 			<div class="wrap_menu">
 				<nav class="menu">
-				<?php 
-				if( has_nav_menu( 'primary-menu' ) ){
-				$args = array(
-					'theme_location' => 'primary-menu',
-					'container' 	 => '',
-					'depth'			 => 2,
-					'menu_class'     => 'main_menu',
-					'fallback_cb'    => 'fashe_bootstrap_navwalker::fallback',
-					'walker'    	 => new fashe_bootstrap_navwalker(),
-				);  
-				wp_nav_menu( $args );
+				<?php
+				if ( has_nav_menu( 'primary-menu' ) ) {
+					$args = array(
+						'theme_location' => 'primary-menu',
+						'container'      => '',
+						'depth'          => 2,
+						'menu_class'     => 'main_menu',
+						'fallback_cb'    => 'fashe_bootstrap_navwalker::fallback',
+						'walker'         => new fashe_bootstrap_navwalker(),
+					);
+					wp_nav_menu( $args );
 				}
 				?>
 				</nav>
@@ -51,32 +51,32 @@
 		</div>
 
 		<div class="bottombar flex-col-c p-b-65">
-			<?php 
-			$socialMenu = fashe_opt('fashe-headersocial-toggle-settings', true );
+			<?php
+			$social_menu = fashe_opt( 'fashe-headersocial-toggle-settings', true );
 			// Social Media
-			if( $socialMenu ){
-				if( has_nav_menu('social-menu') ){
+			if ( $social_menu ) {
+				if ( has_nav_menu( 'social-menu' ) ) {
 					echo '<div class="bottombar-social t-center p-b-8">';
 					$args = array(
 						'theme_location' => 'social-menu',
-						'container' => '',
+						'container'      => '',
 						'depth'          => 1,
 						'fallback_cb'    => 'fashe_social_navwalker::fallback',
-						'walker'    	 => new fashe_social_navwalker(),
-					);  
+						'walker'         => new fashe_social_navwalker(),
+					);
 					wp_nav_menu( $args );
 					echo '</div>';
 				}
 			}
 			//
-			$headerTranslate = fashe_opt('fashe-headerTranslate-toggle-settings');
-			if( $headerTranslate ):
-			?>
+			$header_translate = fashe_opt( 'fashe-headerTranslate-toggle-settings' );
+			if ( $header_translate ) :
+				?>
 
 			<div class="bottombar-child2 p-r-20">
-				<div id="weglot_here"></div> 
+				<div id="weglot_here"></div>
 			</div>
-			<?php 
+				<?php
 			endif;
 			?>
 		</div>
@@ -84,31 +84,31 @@
 
 	<!-- Header Mobile -->
 	<div class="wrap_header_mobile">
-		<?php 
+		<?php
 		// Logo moblie
-		echo fashe_theme_logo('logo-mobile');
+		echo fashe_theme_logo( 'logo-mobile' );
 		?>
 
 		<div class="btn-show-menu">
 			<!-- Header Icon mobile -->
 			<div class="header-icons-mobile">
-				<?php 
-				if( $loginUrl ):
-				?>
-				<a href="<?php echo esc_url( $loginUrl ); ?>" class="header-wrapicon1 dis-block">
-					<img src="<?php echo esc_url( FASHE_DIR_ASSETS_URI ) ?>img/icons/icon-header-01.png" class="header-icon1" alt="<?php esc_html_e( 'ICON', 'fashe' ); ?>">
-				</a>
 				<?php
-				endif; 
+				if ( $login_url ) :
+					?>
+				<a href="<?php echo esc_url( $login_url ); ?>" class="header-wrapicon1 dis-block">
+					<img src="<?php echo esc_url( FASHE_DIR_ASSETS_URI ); ?>img/icons/icon-header-01.png" class="header-icon1" alt="<?php esc_html_e( 'ICON', 'fashe' ); ?>">
+				</a>
+					<?php
+				endif;
 				// Divider
-				if( $cart && $loginUrl ){
+				if ( $cart && $login_url ) {
 					echo '<span class="linedivide1"></span>';
 				}
 				?>
 				<span class="linedivide2"></span>
 
-				<?php 
-				if( $cart ){
+				<?php
+				if ( $cart ) {
 					fashe_cart_count();
 				}
 				?>
@@ -127,78 +127,78 @@
 	<div class="wrap-side-menu" >
 		<nav class="side-menu">
 			<ul class="main-menu">
-				<?php 
+				<?php
 				// Header Top Text
-				$text = fashe_opt('fashe_header_top_text');
+				$text = fashe_opt( 'fashe_header_top_text' );
 				//  Header Translate
-				$headerTranslate = fashe_opt('fashe-headerTranslate-toggle-settings');
+				$header_translate = fashe_opt( 'fashe-headerTranslate-toggle-settings' );
 				// Email
 				$email = fashe_opt( 'fashe_header_top_email' );
 				//
-				if( $text ):
-				?>
+				if ( $text ) :
+					?>
 				<li class="item-topbar-mobile p-l-20 p-t-8 p-b-8">
 					<span class="topbar-child1"><?php echo esc_html( $text ); ?></span>
 				</li>
-				<?php 
+					<?php
 				endif;
 				//
-				if( $headerTranslate || $email ):
-				?>
+				if ( $header_translate || $email ) :
+					?>
 
 				<li class="item-topbar-mobile p-l-20 p-t-8 p-b-8">
 					<div class="topbar-child2-mobile">
-						<?php 
-						if( $email ):
-						?>
+						<?php
+						if ( $email ) :
+							?>
 						<span class="topbar-email"><?php echo esc_html( $email ); ?></span>
-						<?php 
+							<?php
 						endif;
 						//
-						if( $headerTranslate ):
-						?>
-						<div id="weglot_here"></div> 
-						<?php 
+						if ( $header_translate ) :
+							?>
+						<div id="weglot_here"></div>
+							<?php
 						endif;
 						?>
 					</div>
 				</li>
-				<?php 
+					<?php
 				endif;
 				// Social Media
-				if( $socialMenu ):
-				?>
+				if ( $social_menu ) :
+					?>
 				<li class="item-topbar-mobile p-l-10">
-					<?php 
-					
-						if( has_nav_menu('social-menu') ){
-							echo '<div class="topbar-social-mobile">';
-							$args = array(
-								'theme_location' => 'social-menu',
-								'container' => '',
-								'depth'          => 1,
-								'fallback_cb'    => 'fashe_social_navwalker::fallback',
-								'walker'    	 => new fashe_social_navwalker(),
-							);  
-							wp_nav_menu( $args );
-							echo '</div>';
-						}
+					<?php
+
+					if ( has_nav_menu( 'social-menu' ) ) {
+						echo '<div class="topbar-social-mobile">';
+						$args = array(
+							'theme_location' => 'social-menu',
+							'container'      => '',
+							'depth'          => 1,
+							'fallback_cb'    => 'fashe_social_navwalker::fallback',
+							'walker'         => new fashe_social_navwalker(),
+						);
+						wp_nav_menu( $args );
+						echo '</div>';
+					}
 					?>
 				</li>
-				<?php 
+					<?php
 				endif;
 				?>
 			</ul>
-			<?php 
-			if( has_nav_menu( 'primary-menu' ) ){
+			<?php
+			if ( has_nav_menu( 'primary-menu' ) ) {
 				$args = array(
 					'theme_location' => 'primary-menu',
-					'container' 	 => '',
-					'depth'			 => 2,
+					'container'      => '',
+					'depth'          => 2,
 					'menu_class'     => 'main-menu mobile-main-menu',
 					'fallback_cb'    => 'fashe_mobile_bootstrap_navwalker::fallback',
-					'walker'    	 => new fashe_mobile_bootstrap_navwalker(),
-				);  
+					'walker'         => new fashe_mobile_bootstrap_navwalker(),
+				);
 				wp_nav_menu( $args );
 			}
 			?>
